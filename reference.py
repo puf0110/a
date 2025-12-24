80d7238f706a1f21afca643a87e7d2a72ec7b668
paladin0320 <cecildyelite@gmail.com>
2025-12-24 22:58:17 +1000

    reference data 2

---
+++ reference.py
@@ -0,0 +1,482 @@
+[
+    {
+        "id": 1,
+        "event_title": "Will Al Fayha Saudi Club win on 2025-12-25?",
+        "score": 0.99
+    },
+    {
+        "id": 2,
+        "event_title": "Will Al Fayha Saudi Club vs. Al Hazem SC end in a draw?",
+        "score": 0.01
+    },
+    {
+        "id": 3,
+        "event_title": "Will Al Hazem SC win on 2025-12-25?",
+        "score": 0.01
+    },
+    {
+        "id": 4,
+        "event_title": "Will Al Riyadh Saudi Club win on 2025-12-25?",
+        "score": 0.3456789
+    },
+    {
+        "id": 5,
+        "event_title": "Will Al Ettifaq Saudi Club win on 2025-12-25?",
+        "score": 0.3456789
+    },
+    {
+        "id": 6,
+        "event_title": "Will NEOM SC win on 2025-12-25?",
+        "score": 0.3456789
+    },
+    {
+        "id": 7,
+        "event_title": "Will NEOM SC vs. Al Najmah Saudi Club end in a draw?",
+        "score": 0.3456789
+    },
+    {
+        "id": 8,
+        "event_title": "Will Al Najmah Saudi Club win on 2025-12-25?",
+        "score": 0.3456789
+    },
+    {
+      "id": 9,
+      "event_title": "Will the price of Ethereum be above $2,400 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 10,
+      "event_title": "Will the price of Ethereum be above $2,500 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 11,
+      "event_title": "Will the price of Ethereum be above $2,600 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 12,
+      "event_title": "Will the price of Ethereum be above $2,700 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 13,
+      "event_title": "Will the price of Ethereum be above $2,800 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 14,
+      "event_title": "Will the price of Ethereum be above $2,900 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 15,
+      "event_title": "Will the price of Ethereum be above $3,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 16,
+      "event_title": "Will the price of Ethereum be above $3,100 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 17,
+      "event_title": "Will the price of Ethereum be above $3,200 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 18,
+      "event_title": "Will the price of Ethereum be above $3,300 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 19,
+      "event_title": "Will the price of Ethereum be above $3,400 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 20,
+      "event_title": "Will the price of Bitcoin be less than $80,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 21,
+      "event_title": "Will the price of Bitcoin be between $80,000 and $82,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 22,
+      "event_title": "Will the price of Bitcoin be between $82,000 and $84,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 23,
+      "event_title": "Will the price of Bitcoin be between $84,000 and $86,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 24,
+      "event_title": "Will the price of Bitcoin be between $94,000 and $96,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 25,
+      "event_title": "Will the price of Bitcoin be between $96,000 and $98,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 26,
+      "event_title": "Will the price of Bitcoin be between $86,000 and $88,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 27,
+      "event_title": "Will the price of Bitcoin be greater than $98,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 28,
+      "event_title": "Will the price of Bitcoin be between $88,000 and $90,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 29,
+      "event_title": "Will the price of Bitcoin be between $90,000 and $92,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 30,
+      "event_title": "Will the price of Bitcoin be between $92,000 and $94,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 31,
+      "event_title": "Will the price of Bitcoin be above $78,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 32,
+      "event_title": "Will the price of Bitcoin be above $80,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 33,
+      "event_title": "Will the price of Bitcoin be above $82,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 34,
+      "event_title": "Will the price of Bitcoin be above $84,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 35,
+      "event_title": "Will the price of Bitcoin be above $86,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 36,
+      "event_title": "Will the price of Bitcoin be above $88,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 37,
+      "event_title": "Will the price of Bitcoin be above $90,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 38,
+      "event_title": "Will the price of Bitcoin be above $92,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 39,
+      "event_title": "Will the price of Bitcoin be above $94,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 40,
+      "event_title": "Will the price of Bitcoin be above $96,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 41,
+      "event_title": "Will the price of Bitcoin be above $98,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 42,
+      "event_title": "Will the price of Ethereum be less than $2,500 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 43,
+      "event_title": "Will the price of Ethereum be between $2,500 and $2,600 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 44,
+      "event_title": "Will the price of Ethereum be between $2,600 and $2,700 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 45,
+      "event_title": "Will the price of Ethereum be between $2,700 and $2,800 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 46,
+      "event_title": "Will the price of Ethereum be between $2,800 and $2,900 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 47,
+      "event_title": "Will the price of Ethereum be between $2,900 and $3,000 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 48,
+      "event_title": "Will the price of Ethereum be between $3,000 and $3,100 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 49,
+      "event_title": "Will the price of Ethereum be between $3,100 and $3,200 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 50,
+      "event_title": "Will the price of Ethereum be between $3,200 and $3,300 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 51,
+      "event_title": "Will the price of Ethereum be between $3,300 and $3,400 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 52,
+      "event_title": "Will the price of Ethereum be greater than $3,400 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 53,
+      "event_title": "Will the price of Solana be above $80 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 54,
+      "event_title": "Will the price of Solana be above $90 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 55,
+      "event_title": "Will the price of Solana be above $100 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 56,
+      "event_title": "Will the price of Solana be above $110 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 57,
+      "event_title": "Will the price of Solana be above $120 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 58,
+      "event_title": "Will the price of Solana be above $130 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 59,
+      "event_title": "Will the price of Solana be above $140 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 60,
+      "event_title": "Will the price of Solana be above $150 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 61,
+      "event_title": "Will the price of Solana be above $160 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 62,
+      "event_title": "Will the price of Solana be above $170 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 63,
+      "event_title": "Will the price of Solana be above $180 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 64,
+      "event_title": "Will the price of Solana be less than $80 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 65,
+      "event_title": "Will the price of Solana be between $80 and $90 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 66,
+      "event_title": "Will the price of Solana be between $90 and $100 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 67,
+      "event_title": "Will the price of Solana be between $100 and $110 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 68,
+      "event_title": "Will the price of Solana be between $110 and $120 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 69,
+      "event_title": "Will the price of Solana be between $120 and $130 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 70,
+      "event_title": "Will the price of Solana be between $130 and $140 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 71,
+      "event_title": "Will the price of Solana be between $140 and $150 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 72,
+      "event_title": "Will the price of Solana be between $150 and $160 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 73,
+      "event_title": "Will the price of Solana be between $160 and $170 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 74,
+      "event_title": "Will the price of Solana be greater than $170 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 75,
+      "event_title": "Will the price of XRP be above $2.10 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 76,
+      "event_title": "Will the price of XRP be above $2.20 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 77,
+      "event_title": "Will the price of XRP be above $2.30 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 78,
+      "event_title": "Will the price of XRP be above $2.40 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 79,
+      "event_title": "Will the price of XRP be above $1.40 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 80,
+      "event_title": "Will the price of XRP be above $1.50 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 81,
+      "event_title": "Will the price of XRP be above $1.60 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 82,
+      "event_title": "Will the price of XRP be above $1.70 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 83,
+      "event_title": "Will the price of XRP be above $1.80 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 84,
+      "event_title": "Will the price of XRP be above $1.90 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 85,
+      "event_title": "Will the price of XRP be above $2.00 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 86,
+      "event_title": "Will the price of XRP be less than $1.50 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 87,
+      "event_title": "Will the price of XRP be between $1.50 and $1.60 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 88,
+      "event_title": "Will the price of XRP be between $1.60 and $1.70 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 89,
+      "event_title": "Will the price of XRP be between $1.70 and $1.80 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 90,
+      "event_title": "Will the price of XRP be between $1.80 and $1.90 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 91,
+      "event_title": "Will the price of XRP be between $1.90 and $2.00 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 92,
+      "event_title": "Will the price of XRP be between $2.00 and $2.10 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 93,
+      "event_title": "Will the price of XRP be between $2.10 and $2.20 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 94,
+      "event_title": "Will the price of XRP be between $2.20 and $2.30 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 95,
+      "event_title": "Will the price of XRP be between $2.30 and $2.40 on December 25?",
+      "score": 0.3456789
+    },
+    {
+      "id": 96,
+      "event_title": "Will the price of XRP be greater than $2.40 on December 25?",
+      "score": 0.3456789
+    }
+]
